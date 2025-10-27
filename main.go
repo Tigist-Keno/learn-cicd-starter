ee3ba372bbd27a00be938b4318e8ef7a189ab2d4
Tigist Keno <tigistkeno@tigists-air.speedport.ip>
2025-10-27 22:55:25 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello from Tigist's Go server!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Server listening on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
+
